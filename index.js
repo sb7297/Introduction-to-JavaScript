33121c17a0577089600350715f8b5603bd372f0b
sb7297 <sbaireddy7297@gmail.com>
2021-06-07 18:49:27 -0400

    index.js: complete task 2

--- index.js
+++ index.js
@@ -72,12 +72,9 @@ Do the following:
    2. Use the received value to calculate the age in dog years (1 human year is equal to 7 dog years)
    3. Return the newly calculated age
 */
-
-function dogYears(/*add your code here*/){
-    /*add your code here*/
+function dogYears(humanYears){
+  return humanYears*7;
 }
-
-
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
